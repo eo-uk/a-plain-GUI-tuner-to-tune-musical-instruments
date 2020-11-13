0e132df064cf405d4abe5bcec34a122712e9a89f
eo-uk <64873688+eo-uk@users.noreply.github.com>
2020-11-13 14:18:40 +0000

    Update tuner.py

--- tuner.py
+++ tuner.py
@@ -1,1 +1,14 @@
+import os
+import sounddevice as sd
+import pitch
+from scipy.io.wavfile import write
 
+
+def get_current_pitch():
+    fs = 44100  
+    seconds = 0.1
+    myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=1)
+    sd.wait()  
+    write('audio', fs, myrecording)   
+    
+    return pitch.find_pitch('audio')
