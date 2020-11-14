046eea4fa732d2d5da292205fd3ed32a9c0ec113
eo-uk <64873688+eo-uk@users.noreply.github.com>
2020-11-14 00:56:48 +0000

    Update tuner.py

--- tuner.py
+++ tuner.py
@@ -12,3 +12,16 @@ def get_current_pitch():
     write('audio', fs, myrecording)   
     
     return pitch.find_pitch('audio')
+
+def check(pitch_target):
+    #Get current pitch from the microphone input
+    pitch_current = get_current_pitch()
+    #Get delta between current pitch and target pitch
+    delta = int(pitch_current - pitch_target)
+
+    #Text to display above the offset
+    text = 'Too Low' if delta < -10 else 'Too High' if delta > 10 else 'Perfect!'
+    #Decorate the offset with a plus sign if above 0
+    offset = '+' + str(delta) if delta > 0 else str(delta)
+
+    return (text, offset)
